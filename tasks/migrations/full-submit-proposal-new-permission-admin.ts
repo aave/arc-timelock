e7dd1081a01a481e8daa9e56edd5e781c6b8d676
miguelmtzinf <miguelmtz.mail@gmail.com>
2022-02-04 09:58:24 +0100

    feat: Add tests and tasks for adding new permission admin via proposal

---
+++ tasks/migrations/full-submit-proposal-new-permission-admin.ts
@@ -0,0 +1,48 @@
+import { task } from 'hardhat/config';
+import { DRE } from '../../helpers/misc-utils';
+import { IAaveGovernanceV2__factory } from '../../typechain/factories/IAaveGovernanceV2__factory';
+
+const {
+  AAVE_GOVERNANCE_ADDRESS,
+  AAVE_SHORT_EXECUTOR_ADDRESS,
+  ARC_PERMISSION_MANAGER_ADDRESS,
+  ARC_TIMELOCK_ADDRESS,
+  PROPOSAL_PAYLOAD_ADDRESS,
+  IPFS_ENCODED_HASH,
+} = process.env;
+
+task('full:submit-proposal-new-permission-admin', 'Submit Proposal')
+  .addParam('admin')
+  .addParam('ipfs')
+  .setAction(async ({ admin, ipfs }, hre) => {
+    if (
+      !AAVE_GOVERNANCE_ADDRESS ||
+      !AAVE_SHORT_EXECUTOR_ADDRESS ||
+      !ARC_TIMELOCK_ADDRESS ||
+      !ARC_PERMISSION_MANAGER_ADDRESS ||
+      !PROPOSAL_PAYLOAD_ADDRESS ||
+      !IPFS_ENCODED_HASH
+    ) {
+      throw new Error(
+        'You have not set correctly the .config.env file, make sure to read the README.md'
+      );
+    }
+    await hre.run('set-DRE');
+
+    const proposalId = await hre.run('submit-proposal-new-permission-admin', {
+      defender: process.env.DEFENDER === 'true',
+      aaveGovernance: AAVE_GOVERNANCE_ADDRESS,
+      permissionManager: ARC_PERMISSION_MANAGER_ADDRESS,
+      executor: AAVE_SHORT_EXECUTOR_ADDRESS,
+      timelock: ARC_TIMELOCK_ADDRESS,
+      newPermissionAdmin: admin,
+      ipfsEncoded: ipfs,
+    });
+
+    const govContract = IAaveGovernanceV2__factory.connect(
+      AAVE_GOVERNANCE_ADDRESS,
+      (await (DRE as any).ethers.getSigners())[0]
+    );
+    console.log('Proposal submitted:', await govContract.getProposalById(proposalId));
+    return proposalId;
+  });
